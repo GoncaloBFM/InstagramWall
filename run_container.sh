71f1ebf4b0d23a6ae1e5988b24ea7bede1d18e65
gbfm <goncalo.bfm@gmail.com>
2019-07-03 05:18:58 +0100

    Cleanup

--- run_container.sh
+++ run_container.sh
@@ -2,4 +2,5 @@ docker run \
 --mount type=bind,source=$(pwd)/src,target=/InstagramWall/src \
 --mount type=bind,source=$(pwd)/lib,target=/InstagramWall/lib \
 --mount type=bind,source=$(pwd)/data,target=/InstagramWall/data \
+--mount type=bind,source=$(pwd)/logs,target=/InstagramWall/logs \
 -it wall
